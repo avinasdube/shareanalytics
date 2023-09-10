70910a9d3d10e90ea08ebba3eb9645b23875224a
Vishruth <vishruthvs2003@gmail.com>
2023-09-10 13:57:43 +0530

    [CREATED]: Created basic structure for login page

--- sa-client/src/components/molecules/LoginForm/LoginForm.tsx
+++ sa-client/src/components/molecules/LoginForm/LoginForm.tsx
@@ -1,28 +1,35 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const LoginForm: React.FC = () => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
   const handleLogin = () => {
     // Logic for handling login
   };
 
   return (
-    <div className="login-form">
-      <input
-        type="text"
-        placeholder="Username"
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
-      />
-      <input
-        type="password"
-        placeholder="Password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <button onClick={handleLogin}>Login</button>
+    <div className="login-container">
+      <div className="login-box">
+        <h2 className="form-title">Login</h2>
+        <input
+          type="text"
+          placeholder="Username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          className="input-field"
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          className="input-field"
+        />
+        <button className="login-button" onClick={handleLogin}>
+          Login
+        </button>
+      </div>
     </div>
   );
 };
