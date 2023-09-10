3d232d9194fd3205f8e39c8a692c4049361b29c1
Vishruth <vishruthvs2003@gmail.com>
2023-09-10 23:03:13 +0530

    [CREATED]:Login containing otp

--- sa-client/src/components/molecules/LoginForm/LoginForm.tsx
+++ sa-client/src/components/molecules/LoginForm/LoginForm.tsx
@@ -1,37 +1,71 @@
+// PhoneOTPContainer.tsx
+
 import React, { useState } from "react";
+import "./LoginForm.scss"; // Import the SCSS file
 
-const LoginForm: React.FC = () => {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
+const PhoneOTPContainer: React.FC = () => {
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [showOTPContainer, setShowOTPContainer] = useState(false);
+  const [agreeToTerms, setAgreeToTerms] = useState(false);
 
-  const handleLogin = () => {
-    // Logic for handling login
+  const handlePhoneNumberChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPhoneNumber(e.target.value);
+  };
+
+  const handleGetOTP = () => {
+    // Logic for sending OTP to the entered phone number
+    setShowOTPContainer(true);
+  };
+
+  const handleCancel = () => {
+    // Logic for canceling OTP request
+    setShowOTPContainer(false);
+  };
+
+  const handleAgreeToTermsChange = () => {
+    // Toggle the agreeToTerms state
+    setAgreeToTerms(!agreeToTerms);
   };
 
   return (
-    <div className="login-container">
-      <div className="login-box">
-        <h2 className="form-title">Login</h2>
+    <div className="phone-otp-container">
+      <p>Enter Phone Number</p>
+      {!showOTPContainer ? (
+        <div className="phone-input-container">
+          <img src="/phone-icon.svg" alt="Phone Icon" className="phone-icon" />
+          <input
+            type="text"
+            placeholder="Your Phone Number"
+            value={phoneNumber}
+            onChange={handlePhoneNumberChange}
+            className="phone-input"
+          />
+          <button className="get-otp-button" onClick={handleGetOTP}>
+            Get OTP
+          </button>
+        </div>
+      ) : (
+        <div className="otp-container">
+          <div className="otp-header">
+            <span>OTP Sent to {phoneNumber}</span>
+            <button className="close-button" onClick={handleCancel}>
+              &#x2716;
+            </button>
+          </div>
+          {/* Display OTP input and verification button here */}
+        </div>
+      )}
+      {/* New container for terms and conditions */}
+      <div className="terms-container">
         <input
-          type="text"
-          placeholder="Username"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-          className="input-field"
+          type="checkbox"
+          checked={agreeToTerms}
+          onChange={handleAgreeToTermsChange}
         />
-        <input
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          className="input-field"
-        />
-        <button className="login-button" onClick={handleLogin}>
-          Login
-        </button>
+        <span>I agree to the terms and conditions</span>
       </div>
     </div>
   );
 };
 
-export default LoginForm;
+export default PhoneOTPContainer;
