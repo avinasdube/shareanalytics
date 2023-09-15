bf285c35b5fad5a67105d036b998d70e104c0834
Deeksha Rungta <deeksharu1902@gmail.com>
2023-09-15 21:28:31 +0530

    "[new]: added functionality to the login button"

--- sa-client/src/components/pages/Login/Login.tsx
+++ sa-client/src/components/pages/Login/Login.tsx
@@ -1,5 +1,6 @@
 import React, { FormEvent, useState } from "react";
 import "./Login.scss";
+import Modal from "../../ui/Modal";
 const Logo: string = require("../../../assets/icons/logo-2.png");
 
 type FormData = {
@@ -7,68 +8,79 @@ type FormData = {
   password: string;
 };
 
+interface LoginProps {
+  onClose?: () => void;
+}
+
 const initialFormData: FormData = {
   email: "",
   password: "",
 };
 
-const Login: React.FC = () => {
+const Login: React.FC<LoginProps> = ({ onClose }) => {
   const [formData, setFormData] = useState<FormData>(initialFormData);
-  
+
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     console.log("submit login goes here");
     console.log(formData);
+    onClose && onClose();
   };
-  
+
   const handleChage = (e: React.ChangeEvent<HTMLInputElement>) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };
-  
+
+  const handleCloseButton = () => {
+    onClose && onClose();
+  };
+
   return (
-    <main className="main loginMain">
-      <section className="content">
-        <div className="contentIntro">
-          <div className="contentIntroTitle">
-            <img src={Logo} alt="Logo" />
-          </div>
-          <p className="contentIntroDesc">
-            Please input login details to gain access.
-          </p>
-        </div>
-
-        <form onSubmit={handleSubmit}>
-          <div className="inputBox">
-            <i className="inputBox--icon bx bx-envelope"></i>
-            <input
-              name="email"
-              placeholder="Enter your email address"
-              type="email"
-              required
-              value={formData.email}
-              onChange={handleChage}
-            />
+    <Modal onClose={onClose}>
+      <main className="main loginMain">
+        <section className="content">
+          <div className="contentIntro">
+            <div className="contentIntroTitle">
+              <img src={Logo} alt="Logo" />
+              <button className="close-btn" onClick={handleCloseButton}>
+                <i className=" bx bx-x"></i>
+              </button>
+            </div>
+            <p className="contentIntroDesc">
+              Please input login details to gain access.
+            </p>
           </div>
 
-          <div className="inputBox">
-            <i className="inputBox--icon bx bx-lock-alt"></i>
-            <input
-              name="password"
-              placeholder="Enter your passsword"
-              type="password"
-              required
-              value={formData.password}
-              onChange={handleChage}
-            />
-          </div>
+          <form onSubmit={handleSubmit}>
+            <div className="inputBox">
+              <i className="inputBox--icon bx bx-envelope"></i>
+              <input
+                name="email"
+                placeholder="Enter your email address"
+                type="email"
+                required
+                value={formData.email}
+                onChange={handleChage}
+              />
+            </div>
 
-          <button type="submit">
-            Log In
-          </button>
-        </form>
+            <div className="inputBox">
+              <i className="inputBox--icon bx bx-lock-alt"></i>
+              <input
+                name="password"
+                placeholder="Enter your passsword"
+                type="password"
+                required
+                value={formData.password}
+                onChange={handleChage}
+              />
+            </div>
 
-      </section>
-    </main>
+            <button type="submit">Log In</button>
+          </form>
+        </section>
+      </main>
+    </Modal>
   );
 };
 
